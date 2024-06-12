d5a43231ab12371f55564acdfd5092a535dc1089
Cassio Reinaldo Amaral <codigoalvo@gmail.com>
2024-06-12 15:08:04 -0300

    Completa métodos do CRUD (ainda de forma rudimentar) e faz algumas melhorias.

--- src/main/kotlin/br/com/codigoalvo/kotlinmsg/controller/MessageController.kt
+++ src/main/kotlin/br/com/codigoalvo/kotlinmsg/controller/MessageController.kt
@@ -2,18 +2,33 @@ package br.com.codigoalvo.kotlinmsg.controller
 
 import br.com.codigoalvo.kotlinmsg.model.Message
 import br.com.codigoalvo.kotlinmsg.service.MessageService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/v1/messages")
 class MessageController(val service: MessageService) {
+
     @GetMapping
-    fun index(): List<Message> = service.findMessages()
+    fun listAll(): ResponseEntity<*>? =
+        ResponseEntity.ok(service.listAll())
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable id: String): ResponseEntity<*>? =
+        ResponseEntity.ok(service.getById(id))
 
     @PostMapping
-    fun post(@RequestBody message: Message) {
-        service.post(message)
+    fun post(@RequestBody message: Message): ResponseEntity<*> =
+        ResponseEntity.ok(service.post(message))
+
+    @PutMapping("/{id}")
+    fun put(@PathVariable id: String, @RequestBody message: Message): ResponseEntity<*> {
+        return ResponseEntity.ok(service.put(Message(id, message.content)))
     }
+
+    @DeleteMapping("/{id}")
+    fun deleteById(@PathVariable id: String): ResponseEntity<*>? =
+        ResponseEntity.ok(service.deleteById(id))
+
+
 }
--- src/main/kotlin/br/com/codigoalvo/kotlinmsg/service/MessageService.kt
+++ src/main/kotlin/br/com/codigoalvo/kotlinmsg/service/MessageService.kt
@@ -2,15 +2,21 @@ package br.com.codigoalvo.kotlinmsg.service
 
 import br.com.codigoalvo.kotlinmsg.repository.MessageRepository
 import br.com.codigoalvo.kotlinmsg.model.Message
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
 import org.springframework.stereotype.Service
+import org.springframework.web.client.HttpClientErrorException.NotFound
 
 @Service
 class MessageService(val repository: MessageRepository) {
 
-    fun findMessages() :List<Message> = repository.findMessages()
+    fun listAll(): List<Message> = repository.findMessages()
 
-    fun post(message: Message) {
-        repository.save(message)
-    }
+    fun getById(id: String): Message? = repository.findById(id).orElseThrow { NotFoundException() }
+
+    fun post(message: Message): String? = repository.save(message).id
+
+    fun put(message: Message): Message? = repository.save(message)
+
+    fun deleteById(id: String): Unit? = repository.deleteById(id)
 
 }
